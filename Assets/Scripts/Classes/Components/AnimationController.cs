4cf8889f4c3b8c4a1513b8167debe066a95c8426
Tiền Minh Dương <tmd31052005@gmail.com>
2026-05-24 23:15:04 +0700

    Replace awaitable with unitask

--- Assets/Scripts/Classes/Components/AnimationController.cs
+++ Assets/Scripts/Classes/Components/AnimationController.cs
@@ -1,4 +1,5 @@
 using System;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -12,16 +13,16 @@ public class AnimationController
 
     public UnityAction OnOverrideAnimationComplete;
 
-    private async Awaitable CrossFade(AnimationData animationData, float transitionDuration = 0.1f)
+    private async UniTask CrossFade(AnimationData animationData, float transitionDuration = 0.1f)
     {
         // Debug.Log($"Crossfading to state with hash: {animationData.Hash}");
         animator.CrossFade(animationData.Hash, transitionDuration);
-        await Awaitable.WaitForSecondsAsync(transitionDuration + animationData.AnimationDuration);
+        await UniTask.WaitForSeconds(transitionDuration + animationData.AnimationDuration);
     }
 
     private AnimationData currentAnim;
     private AnimationData currentStandardAnim;
-    public async Awaitable PlayAnimation(AnimationData anim, float transitionDuration = 0.1f)
+    public async UniTask PlayAnimation(AnimationData anim, float transitionDuration = 0.1f)
     {
         // Debug.Log($"Playing animation: {anim.stateName} with priority {anim.priority}");
         if (anim.priority == AnimationData.PriorityLevel.Override)
@@ -45,7 +46,7 @@ public class AnimationController
         }
     }
 
-    private async Awaitable RevertStandardAnim(float transitionDuration = 0.1f)
+    private async UniTask RevertStandardAnim(float transitionDuration = 0.1f)
     {
         currentAnim = currentStandardAnim;
         // Debug.Log($"Reverting to standard animation: {currentStandardAnim.stateName}");
--- Assets/Scripts/Classes/Events/ChangeHealthPlayerEvent.cs
+++ Assets/Scripts/Classes/Events/ChangeHealthPlayerEvent.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class ChangeHealthPlayerEvent: IEvent
+public struct ChangeHealthPlayerEvent : IEvent
 {
     public float HealthChange { get; private set; }
     public ChangeHealthPlayerEvent(float healthChange)
